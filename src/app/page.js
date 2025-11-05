7b188897208dbc9667596f96716b57a6f4431224
Jeesun Bari <jeesun.websadroit@gmail.com>
2025-11-05 16:53:17 +0530

    update few things

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="">Hello world!</h1>
+      <h1 className="hero-h1-gradiant font-[500]">Hello world! <br/> Hola</h1>
       <h2>Next Js</h2>
     </>
   );
